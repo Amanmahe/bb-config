25222a5ca832e96621a44efcde673186edd8ec35
Aman Maheshwari <amanmaheshwari715@gmail.com>
2025-12-15 23:27:47 +0530

    Update gpio_impl

--- src/ui/panel/gpio/gpio_impl.cpp
+++ src/ui/panel/gpio/gpio_impl.cpp
@@ -239,7 +239,7 @@ class Gpio : public ComponentBase {
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    std::unique_ptr<FILE, int(*)(FILE*)> pipe(popen(cmd, "r"), pclose);
     if (!pipe) {
         return "";
     }
